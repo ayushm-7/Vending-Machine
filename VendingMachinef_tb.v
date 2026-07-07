c0b3baa8e869e01115c57911a81e295fedc66dac
ayushm-7 <ayushmohanty6699@gmail.com>
2026-07-08 00:43:49 +0530

    Add testbench for VendingMachinef module

---
+++ VendingMachinef_tb.v
@@ -0,0 +1,71 @@
+module VendingMachinef_tb;
+
+reg clk;
+reg[1:0] in;
+reg rst;
+//output
+wire[1:0] out;
+wire[1:0] change;
+wire[2:0] state_led;
+VendingMachinef uut(
+.clk(clk),
+.rst(rst),
+.in(in),
+.out(out),
+.change(change),
+.state_led(state_led)
+);
+initial clk = 0;           // Initialize clock
+ always #5 clk = ~clk;      // Toggle every 5 time units (10 ns period)
+
+ // ================= TEST SEQUENCE =================
+ initial begin
+
+    // Initialize inputs
+    rst = 1;
+    in = 2'b00;
+
+    // Reset phase
+    #20;
+    rst = 0;
+
+    // Insert ₹5
+    #10;
+    in = 2'b01;
+
+    // Insert ₹10 → should dispense item
+    #10;
+    in = 2'b10;
+
+    // Insert invalid coin
+    #10;
+    in = 2'b11;
+
+    // Insert ₹5
+    #10;
+    in = 2'b01;
+
+    // Insert ₹10 → special item
+    #10;
+    in = 2'b10;
+
+    // Insert ₹10
+    #10;
+    in = 2'b10;
+
+    // Insert ₹10 → item + change
+    #10;
+    in = 2'b10;
+
+    // Reset again
+    #10;
+    rst = 1;
+
+    #10;
+    rst = 0;
+
+    #50;
+    $finish;
+ end
+
+endmodule
